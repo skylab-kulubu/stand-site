406702ad3b8b1c2afe52c039c6d2c9748866b355
Emre Uslu <emreuslu256@gmail.com>
2024-07-24 17:49:47 +0300

    Addons.

--- src/pages/DetailPage/detailPage.js
+++ src/pages/DetailPage/detailPage.js
@@ -1,30 +1,33 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import {teams} from '../../utils/data';
-import './detailPage.css'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { teams } from "../../utils/data";
+import "./detailPage.css";
 
 const DetailPage = () => {
-    const { teamId } = useParams();
-    const team = teams.find(t => t.id === parseInt(teamId, 10));
-  
-    if (!team) {
-      return <div>Team not found</div>;
-    }
+  const { teamId } = useParams();
+  const team = teams.find((t) => t.id === parseInt(teamId, 10));
+
+  if (!team) {
+    return <div>Team not found</div>;
+  }
 
   return (
-    <div className='team-detail-container'>
-      <div className='team-logo' style={{ backgroundImage: `url(${team.logo})` }}></div>
-      <div className='team-name'>{team.name}</div>
-      <div className='team-description'>{team.description}</div>
-      <div className='team-projects'>
+    <div className="team-detail-container">
+      <div
+        className="team-logo"
+        style={{ backgroundImage: `url(${team.logo})` }}
+      ></div>
+      <div className="team-name">{team.name}</div>
+      <div className="team-description">{team.description}</div>
+      <div className="team-projects">
         {team.projects.map((project, index) => (
-          <div key={index} className='team-project'>
+          <div key={index} className="team-project">
             {project}
           </div>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default DetailPage;
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,10 +1,10 @@
 export const teams = [
     {
       id: 1,
-      logo: 'https://via.placeholder.com/100',
-      name: 'Ekip Alpha',
-      descshort: 'Kısa Açıklama',
-      description: 'Ekip Alpha açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      logo: 'https://dummyimage.com/100x100/eadaff/000000.png&text=ARGE',
+      name: 'ALGOLAB',
+      descshort: 'ALGOLAB, algoritma ve programlama konusunda temel ve orta düzey eğitimler sunan, ekip çalışması ve competitive programming ile kendini geliştiren bir ekiptir.',
+      description: "ALGOLAB ekibi olarak algoritma ve programlama konusunda kendimizi geliştiren, temel ve orta düzeyde algoritma/CPP eğitimleri veren, insanları yazılım ve algoritma dünyasına hazırlayan bir ekibiz. Bu süreçte ekip olarak çalışmayı da öğreniyoruz. Ekip olarak competitive programming dünyasında emin adımlarla ilerliyoruz. Zaman zaman İnzva'nın veya diğer okulların yarışmalarına katılıyoruz. Ayrıca, her hafta AGC (Algorithm Games Challenge) isminde bir algoritma yarışması da düzenliyoruz. ALGOLAB, üç temel alt ekipten oluşur: Beginner: Başlangıç seviyesinde CPP ve algoritma eğitimi verir. Algolearning: Orta düzey algoritma eğitimi sağlar. Algostdy: Algoritma ve yarışmalar konusunda ekip çalışması odaklı bir eğitim sunar. Bu ekipler sayesinde hem bireysel hem de ekip olarak yeteneklerimizi geliştiriyoruz.",
       projects: [
         'x projesi | x projesi detayları ve açıklamaları',
         'x projesi | x projesi detayları ve açıklamaları',
@@ -13,10 +13,10 @@ export const teams = [
     },
     {
       id: 2,
-      logo: 'https://via.placeholder.com/100',
-      name: 'Ekip Beta',
-      descshort: 'Kısa Açıklama',
-      description: 'Ekip Beta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      logo: 'https://dummyimage.com/100x100/eadaff/000000.png&text=ARGE',
+      name: 'AIRLAB',
+      descshort: "AIR ekibi, yapay zeka alanında yarışmalara katılıyor, eğitimler alıyor ve projeler geliştirerek SKYLAB'i teknofest'te temsil etmeyi hedefliyor.",
+      description: "AIR ekibi olarak yapay zeka alanında çalışmalar yapıyoruz. Makine öğrenmesi, derin öğrenme gibi konularla uğraşmakta ve bu yönde yarışmalara katılmaktayız. Şu anda alt ekip olarak ulaşımda yapay zeka yarışmasında yer alıyoruz. Hedefimiz SKYLAB'i teknofestte en iyi şekilde temsil etmek. Bu sene ek olarak iki yarışmaya daha katılmak istiyoruz, bunlar türkçe doğal dil işleme ve sağlıkta yapay zeka. Aynı şekilde kaggle gibi platformlardan datathonlara katılmaya devam edeceğiz. Yaz döneminde ise ekipçe kendimizi geliştirmek adına eğitimler alacağız ve ARTLAB'de sergilemek istediğimiz projelerin üretimine başlayacağız.",
       projects: [
         'x projesi | x projesi detayları ve açıklamaları',
         'x projesi | x projesi detayları ve açıklamaları',
@@ -25,10 +25,10 @@ export const teams = [
     },
     {
       id: 3,
-      logo: 'https://via.placeholder.com/100',
-      name: 'Ekip Gamma',
-      descshort: 'Kısa Açıklama',
-      description: 'Ekip Gamma açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      logo: 'https://dummyimage.com/100x100/eadaff/000000.png&text=ARGE',
+      name: 'CHAINLAB',
+      descshort: 'ChainLab ekibi, blockchain teknolojisini anlayarak web1, web2 ve web3 alanlarında projeler geliştirir, hackathonlara katılır ve ekosistemi tanıtmaya çalışır.',
+      description: 'ChainLab ekibi olarak blokchain ve teknolojisini anlayarak web1 web2 web3 alanlarında kendimizi geliştirmeyi, projeler çıkarmayı, hackathonlara katılmayı ve daha fazla insanın bu ekosistemi tanımasını sağlamayı amaçlıyoruz. Önümüzdeki hackathonlara hazırlanıyoruz ve ekipler çıkarmaya ve adımızı daha çok duyurmaya çalışıyoruz.',
       projects: [
         'x projesi | x projesi detayları ve açıklamaları',
         'x projesi | x projesi detayları ve açıklamaları',
@@ -37,10 +37,10 @@ export const teams = [
     },
     {
       id: 4,
-      logo: 'https://via.placeholder.com/100',
-      name: 'Ekip Delta',
-      descshort: 'Kısa Açıklama',
-      description: 'Ekip Delta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      logo: 'https://dummyimage.com/100x100/eadaff/000000.png&text=ARGE',
+      name: 'GAMELAB',
+      descshort: 'GAMELAB ekibi, oyun geliştirme üzerine çalışarak topluluk etkinlikleri düzenler ve çeşitli platformlar için oyunlar geliştirir, bu yıl yarışmalara katılmayı planlar.',
+      description: 'GAMELAB ekibi olarak oyun geliştirme alanında çalışmalar yapıyoruz. Etkinlikler ve eğitimlerle, insanların bu alana girmesine vesile olup bir arada toplanabilecekleri bir topluluk sağlıyoruz. Alt ekiplere bölünmüş şekilde çeşitli platformlar için oyunlar geliştiriyoruz. Bu sene oyun geliştirme yarışmalarına ekipçe veya bölünmüş birkaç ekip olarak katılmayı planlıyoruz..',
       projects: [
         'x projesi | x projesi detayları ve açıklamaları',
         'x projesi | x projesi detayları ve açıklamaları',
@@ -49,10 +49,10 @@ export const teams = [
     },
     {
       id: 5,
-      logo: 'https://via.placeholder.com/100',
-      name: 'Ekip Epsilon',
-      descshort: 'Kısa Açıklama',
-      description: 'Ekip Epsilon açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      logo: 'https://dummyimage.com/100x100/eadaff/000000.png&text=ARGE',
+      name: 'MOBILAB',
+      descshort: 'MOBILAB ekibi, mobil uygulama projeleri geliştirir, yeni ekip arkadaşları yetiştirir ve okul ve kulübü Teknofest gibi etkinliklerde en iyi şekilde temsil etmeyi hedefler.',
+      description: 'Biz MOBILAB ekibi olarak kulübümüzün ve okulumuzun mobil uygulama ihtiyaçlarını karşılamak için Flutter ile mobil uygulama projeleri yürütüyoruz. Aynı zamanda mobil programlamaya ilgi duyan ve bu alanda yeni olan arkadaşlarımızı projelerde yer almaya hazır olacak şekilde yetiştirerek yeni ekip arkadaşları edinmeye can atıyoruz. Okulumuzu ve kulübümüzü Teknofest gibi ulusal ve uluslararası yarışma ve etkinliklerde en iyi şekilde temsil etmeyi ve yaratıcı uygulama fikirlerini hayata geçirmeyi hedefliyoruz.',
       projects: [
         'x projesi | x projesi detayları ve açıklamaları',
         'x projesi | x projesi detayları ve açıklamaları',
@@ -61,8 +61,8 @@ export const teams = [
     },
     {
       id: 6,
-      logo: 'https://via.placeholder.com/100',
-      name: 'Ekip Zeta',
+      logo: 'https://dummyimage.com/100x100/eadaff/000000.png&text=ARGE',
+      name: 'SKYSIS',
       descshort: 'Kısa Açıklama',
       description: 'Ekip Zeta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
       projects: [
@@ -73,10 +73,10 @@ export const teams = [
     },
     {
       id: 7,
-      logo: 'https://via.placeholder.com/100',
-      name: 'Ekip Eta',
-      descshort: 'Kısa Açıklama',
-      description: 'Ekip Eta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      logo: 'https://dummyimage.com/100x100/eadaff/000000.png&text=ARGE',
+      name: 'SKYSEC',
+      descshort: 'SKY SEC ekibi, siber güvenlik araştırmalarını pentesting, red teaming ve blue teaming üzerine yoğunlaştırarak, Türkiye ve dünya genelinde siber güvenlik etkinliklerinde temsilcilik yapar ve otomasyon güvenlik sistemleri geliştirir.',
+      description: 'SKY SEC ekibi olarak biz 2020 den beri legal olarak ondan önce 2018 yılından beri ekip olmadan siber güvenlik alanında araştırma ekipleri olarak ilerlemekteydi. şu anda ekip dinamiklerine yeni katılan biri pentesting, local pentesting (sızma testi ve kapalı ağ sızma testi) çalışmalarına ardından red teaming(saldırı senaryolarına) ardından blue team senaryoları (savunma senaryoları) üzerine çalışıyor Sosyallik olarak Tryhackme hackthebox stackoverflow, wireshark forum ve underground platformlarda skydays ctf çağrılarıyla Türkiye de hacktrickten sonra siber güvenliğin merkezini İstanbula çekmeyi başardık. bu platformlarda dünyada ve Türkiyede ytü temsilciliği yapmaktayız. Bunların yanında SKY LAB ekipleri ve çalışmaları üzerine otomasyon güvenlik sistemi yazmaktayız. ismimiz siber güvenlik çalışma alanımız Bilgisayar bilimleri diyerek ilerleyen Mottomuz var.',
       projects: [
         'x projesi | x projesi detayları ve açıklamaları',
         'x projesi | x projesi detayları ve açıklamaları',
@@ -85,10 +85,10 @@ export const teams = [
     },
     {
       id: 8,
-      logo: 'https://via.placeholder.com/100',
-      name: 'Ekip Theta',
-      descshort: 'Kısa Açıklama',
-      description: 'Ekip Theta açıklaması, uzun olacak şekilde bir yazı diye düşündük.',
+      logo: 'https://dummyimage.com/100x100/eadaff/000000.png&text=ARGE',
+      name: 'WEBLAB',
+      descshort: 'WEBLAB, kulüp ihtiyaçlarına yönelik React, Next.js, Java ve .NET ile web uygulamaları geliştirir.',
+      description: 'WEBLAB ekibi olarak, web uygulama geliştirme konusunda geniş bir yelpazede projeler yürütüyoruz. Kulübümüzün ihtiyaçlarını karşılamak amacıyla, React, Next.js, Java ve .NET gibi modern teknolojileri kullanarak hem frontend hem de backend geliştirmeleri yapıyoruz. Bu süreçte, çeşitli siteler ve uygulamalar oluşturuyor, kullanıcı deneyimini ve performansı ön planda tutarak etkili çözümler sunuyoruz. Ekip olarak, birçok farklı web uygulaması ve site geliştirerek, kulübümüzün ve diğer ilgili projelerin dijital ihtiyaçlarını karşılamayı hedefliyoruz.',
       projects: [
         'x projesi | x projesi detayları ve açıklamaları',
         'x projesi | x projesi detayları ve açıklamaları',
